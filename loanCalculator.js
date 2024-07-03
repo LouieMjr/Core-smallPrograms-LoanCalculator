f3a1876c3464b9a69baffc4a484e7fb15db7ec27
Louie Mendez <lmendezjr024@gmail.com>
2024-07-03 08:54:23 +0200

    added a function to check if monthly payment is a reasonble one and return a different statement accordingly

--- loanCalculator.js
+++ loanCalculator.js
@@ -38,11 +38,22 @@ const loanDuration = () => {
   return loanDuration;
 };
 
-const calculateMonthlyPayment = (loanAmount, loanDuration, monthlyInterestRate) => {
+const calculateMonthlyPayment = (
+  loanAmount, loanDuration, monthlyInterestRate
+) => {
   console.clear();
   const denominator = 1 - Math.pow((1 + monthlyInterestRate), -loanDuration);
   const monthlyPayment = loanAmount * (monthlyInterestRate / denominator);
   return monthlyPayment;
+};
+
+// having some fun here
+const reasonableMonthlyPayment = (monthlyPayment, totalInterest) => {
+  const scream = `Your monthly payment is $${monthlyPayment.toFixed(2)}!!! \nYour total interest paid over the life of the loan would be $${totalInterest.toFixed(2)}!! \nYou might want to reconsider!`;
+  const reasonable = `Your monthly payment is $${monthlyPayment.toFixed(2)} \nYour total interest paid would be $${totalInterest.toFixed(2)} over the life of the loan.`;
+
+  if (monthlyPayment >= 400) return scream.toUpperCase();
+  return reasonable;
 };
 
 function runLoanCalculator() {
@@ -59,7 +70,7 @@ function runLoanCalculator() {
   let totalPaidAfterInterest = monthlyPayment * loanDurationInMonths;
   totalPaidAfterInterest -= loanAmount;
 
-  return `Your monthly payment is $${monthlyPayment.toFixed(2)} \nYour total interest paid would be $${totalPaidAfterInterest.toFixed(2)} over the life of the loan`;
+  return reasonableMonthlyPayment(monthlyPayment, totalPaidAfterInterest);
 }
 
 console.log(runLoanCalculator());
